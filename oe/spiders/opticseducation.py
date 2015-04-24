2c1eb4834f4dd02086f6e056b06bebd075462c26
Frimann Kjerulf <laukur@gmail.com>
2015-04-24 11:33:51 +0000

    Crawlspider
    Switched to using crawl spider methood

--- oe/spiders/opticseducation.py
+++ oe/spiders/opticseducation.py
@@ -2,13 +2,17 @@
 import scrapy
 
 from oe.items import OeItem
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors import LinkExtractor
 
-class OpticseducationSpider(scrapy.Spider):
+class OpticseducationSpider(CrawlSpider):
     name = "opticseducation"
     allowed_domains = ["www.opticseducation.org"]
     start_urls = (
         'http://www.opticseducation.org/search.aspx?searchmode=anyword&searchstate=0&searchcountry=79&searchdisciplines=&searchdegree=&searchtuition=0&resident=',
     )
+#    rules = [Rule(LinkExtractor(allow=['http://www.opticseducation.org/members/*']), callback='parse_link')]
+    rules = [Rule(LinkExtractor(allow=['http://www.opticseducation.org/members/*']))]
 
     # def parse(self, response):
     #     filename = response.url.split("/")[-2]
@@ -16,8 +20,24 @@ class OpticseducationSpider(scrapy.Spider):
     #         f.write(response.body)
 
     def parse(self, response):
+    	pass
         for sel in response.xpath('//td/a'): #this works!
             item = OeItem()
-            item['title'] = sel.xpath('text()').extract()
-            item['link'] = sel.xpath('@href').extract()
+            item['title'] = sel.xpath('text()').extract()[0].replace('\r\n','').strip()
+            item['link'] = sel.xpath('@href').extract()[0].replace('\r\n','').strip()
             yield item
+
+    def parse_link():
+    	pass
+		# item = OeItem()
+		# item['title'] = sel.xpath('text()').extract()[0].replace('\r\n','').strip()
+		# item['link'] = sel.xpath('@href').extract()[0].replace('\r\n','').strip()
+		# return item
+
+
+
+
+
+
+
+
